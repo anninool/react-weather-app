3a2c4dbe0330d9b75bdba417419783c7294652a4
anninool <anni.nool03@gmail.com>
2022-12-21 12:19:49 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello Mirri!</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <a href="https://github.com/anninool/react-weather-app" target="_blank">
+          Open-source code
+        </a>
+        {""} by Anni Nool
+      </footer>
+    </div>
+  );
 }
