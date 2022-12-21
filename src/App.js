93d721582051371bf6c9cf9f26506ecdf6ecab97
anninool <anni.nool03@gmail.com>
2022-12-21 13:53:57 +0200

    Added HTML structure of the app and search engine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 
 import "./App.css";
 
@@ -6,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
+        <Weather />
         <footer>
           <a
             href="https://github.com/anninool/react-weather-app"
---
+++ src/Weather.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-dark" />
+          </div>
+        </div>
+      </form>
+      <div className="row">
+        <div className="col-2">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-2">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 70%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+        <div className="col-8">
+          <h1>New York </h1>
+          <ul>
+            <li>Wednesday 7:00</li>
+            <li>Mostly cloudy</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
