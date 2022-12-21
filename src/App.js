d4c45e23b4da21853651cba9eefae5b6888b7478
anninool <anni.nool03@gmail.com>
2022-12-21 13:12:39 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        <a href="https://github.com/anninool/react-weather-app" target="_blank">
-          Open-source code
-        </a>
-        {""} by Anni Nool
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          <a
+            href="https://github.com/anninool/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          {""}
+          by Anni Nool
+        </footer>
+      </div>
     </div>
   );
 }
