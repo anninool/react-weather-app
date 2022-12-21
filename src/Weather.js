0979bc91159deb58ca483102b6530fb778610b08
anninool <anni.nool03@gmail.com>
2022-12-21 18:03:45 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -23,27 +23,31 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <div className="row">
-        <div className="col-2">
+      <div className="row mt-3">
+        <div className="col-3">
           <img
             src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
             alt="Mostly cloudy"
           />
-          <span>6</span>°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
-        <div className="col-2">
+        <div className="col-3">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 70%</li>
             <li>Wind: 13km/h</li>
           </ul>
         </div>
-        <div className="col-8">
-          <h1>New York </h1>
-          <ul>
-            <li>Wednesday 7:00</li>
-            <li>Mostly cloudy</li>
-          </ul>
+
+        <div className="col-6 d-flex flex-row-reverse">
+          <div className="weather-card">
+            <h1>New York </h1>
+            <ul>
+              <li>Wednesday 7:00</li>
+              <li>Mostly cloudy</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
