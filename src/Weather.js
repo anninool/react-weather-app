4f19438f1d300422e0c633042f402c76992e11bb
anninool <anni.nool03@gmail.com>
2023-01-02 16:54:32 +0000

    Added functioning Shecodes weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -18,8 +18,7 @@ export default function Weather(props) {
       description: response.data.condition.description,
       date: new Date(response.data.time * 1000),
       icon: response.data.condition.icon,
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
     });
   }
 
